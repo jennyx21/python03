2991857a7bc2e6f9757a9822c540c1c7257db92b
jennyx21 <jtruckse@student.42heilbronn.de>
2026-03-25 01:34:22 +0100

    changed something in the print statement

--- ex1/ft_score_analyticcs.py
+++ ex1/ft_score_analyticcs.py
@@ -3,19 +3,22 @@ import sys
 def ft_score_analytics():
     print("=== Player Score Analytics ===")
     arglist = []
-    try: 
-        for i  in range (1,len(sys.argv)):
-            args = int(sys.argv[i])
-            arglist.append(args)
-        print(f"Scores Processed: {arglist}")
-        print (f"Total Player: {len(arglist)}")
-        print(f"Total Score: {sum(arglist)}")
-        print(f"Average score: {sum(arglist) / len(arglist)}")
-        print(f"High Score: {max(arglist)}")
-        print(f"Low Score: {min(arglist)}")
-        print(f"Score rannge: {max(arglist) - min(arglist)}")
-    except ValueError:
-        print("bad input")
+    if len(sys.argv) > 1:
+        try: 
+            for i  in range (1,len(sys.argv)):
+                args = int(sys.argv[i])
+                arglist.append(args)
+            print(f"Scores Processed: {arglist}")
+            print (f"Total Player: {len(arglist)}")
+            print(f"Total Score: {sum(arglist)}")
+            print(f"Average score: {sum(arglist) / len(arglist)}")
+            print(f"High Score: {max(arglist)}")
+            print(f"Low Score: {min(arglist)}")
+            print(f"Score rannge: {max(arglist) - min(arglist)}")
+        except ValueError:
+            print(f"bad input '{sys.argv[i]}' is not a Number")
+    else:
+        print("No scores provided. Usage: python3 ft_score_analythics.py <score1> <score2> ...")
   
     
 
